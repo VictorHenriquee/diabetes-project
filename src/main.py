8a00c605b416b5bcfee35a786a15f6c53e1c677d
VictorHenriquee <vp.henrique2012@gmail.com>
2025-11-21 21:19:08 -0300

    numpy-removed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
